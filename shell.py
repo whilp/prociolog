3428fb27519c182e34abce483d90acc7d901d0f3
Will Maier <willmaier@ml1.net>
2010-12-03 11:50:01 -0600

    Create a RemoteShell.

--- shell.py
+++ shell.py
@@ -1,18 +1,27 @@
+import pexpect
 import subprocess
 
 from subprocess import PIPE
 
+SHELL = ["/bin/sh"]
+SSH = ["/usr/bin/ssh"]
+
 class Shell(subprocess.Popen):
-    cmd = "/bin/sh"
     defaults = {
         "stdin": PIPE,
         "stdout": PIPE,
         "stderr": PIPE,
     }
 
-    def __init__(self, args=None, **kwargs):
-        if args is None:
-            args = [self.cmd]
+    def __init__(self, args=SHELL, **kwargs):
         defaults = self.defaults.copy()
         defaults.update(kwargs)
         subprocess.Popen.__init__(self, args, **defaults)
+
+class RemoteShell(Shell):
+
+    def __init__(self, args=None, hostname="localhost", **kwargs):
+        if args is None:
+            args = SHELL
+        args = SSH + [hostname] + args
+        Shell.__init__(self, args=args, **kwargs)
